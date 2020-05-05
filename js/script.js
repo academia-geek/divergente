26fe0fb567da18a0d50f8d556af92eb6fd581367
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-05-05 11:17:49 -0500

    cambios

--- js/script.js
+++ js/script.js
@@ -2,16 +2,25 @@ const facciones = [];
 
 let registrarFaccion = (e) => {
 	var valor = document.getElementById("txt_faccion").value; 
-    if(valor.trim() != "" && valor.trim() != null){
-        
-        
+    if(valor.trim() != "" && valor.trim() != null && valor.length == 1){
+        if(facciones.length < 20){
+            facciones.push(valor.toLocaleUpperCase());
+            document.getElementById("txt_faccion").value = "";
+            alert("Facción registrada.");
+        }else{
+            alert("Ya se registro el total de personas");
+        }
     }else{
-        alert("Debe ingresar un valor de facción");
+        alert("Debe ingresar un valor de facción valido");
     }
 }
 
-document.getElementById('btn_enviar_faccion').addEventListener('click', registrarFaccion);
+let calcularTotalFacciones = (e) => {
+    
+}
 
+document.getElementById('btn_enviar_faccion').addEventListener('click', registrarFaccion);
+document.getElementById('calcular_total_facciones').addEventListener('click', calcularTotalFacciones)
 
 let sumar_numeros = (n1, n2) => { 
    let resultado_texto =  `<b>La suma de los dos numeros es:  ${n1 + n2}</b>`;
